42a500d0df5c360dc4e1f9a02cfa5dfeaded6371
danushvale <119531393+danushvale@users.noreply.github.com>
2024-09-04 00:14:23 +0530

    Add files via upload

---
+++ Assignment-2/Assignment1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string uname;
+            string id;
+            int salary;
+            string job;
+
+            Console.WriteLine("enter your Id : ");
+            id = Console.ReadLine();
+
+            Console.WriteLine("enter your name: ");
+            uname = Console.ReadLine();
+
+            Console.WriteLine("enter your Job : ");
+            job = Console.ReadLine();
+
+            Console.WriteLine("enter your Salary : ");
+            salary = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("--------user details----------");
+            Console.WriteLine("ID : " + id);
+            Console.WriteLine("Name : " + uname);
+            Console.WriteLine("Job : " + job);
+            Console.WriteLine("Salary : " + salary);
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Assignment-2/Assignment2.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter Product ID :");
+            int pi = int.Parse( Console.ReadLine());
+
+            Console.WriteLine("enter Product Name:");
+            string pn = Console.ReadLine();
+
+            Console.WriteLine("enter Unit Price :");
+            int up = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("enter Quantity :");
+            int q = int.Parse(Console.ReadLine());
+
+            double total = up * q;
+            double dis;
+            double final;
+
+            if (q >= 10 && q <= 29)
+            {
+                dis = total * 0.1;
+                final = total - (total * 0.1);
+                Console.WriteLine("----------Product Details----------");
+                Console.WriteLine("ID : " + pi);
+                Console.WriteLine("product Name : " + pn);
+                Console.WriteLine("Unit Price : " + up);
+                Console.WriteLine("Quantity : " + q);
+                Console.WriteLine("Total : " + total);
+                Console.WriteLine("Discount : "+dis);
+                Console.WriteLine("Final Amount : " + final);
+            }
+            else if (q >= 30 && q <= 49)
+            {
+                dis = total * 0.2;
+                final = total - (total * 0.2);
+                Console.WriteLine("----------Product Details----------");
+                Console.WriteLine("ID : " + pi);
+                Console.WriteLine("product Name : " + pn);
+                Console.WriteLine("Unit Price : " + up);
+                Console.WriteLine("Quantity : " + q);
+                Console.WriteLine("Total : " + total);
+                Console.WriteLine("Discount : " + dis);
+                Console.WriteLine("Final Amount : " + final);
+            }
+            else if (q >= 50)
+            {
+                dis = total * 0.3;
+                final = total - (total * 0.3);
+                Console.WriteLine("----------Product Details----------");
+                Console.WriteLine("ID : " + pi);
+                Console.WriteLine("product Name : " + pn);
+                Console.WriteLine("Unit Price : " + up);
+                Console.WriteLine("Quantity : " + q);
+                Console.WriteLine("Total : " + total);
+                Console.WriteLine("Discount : " + dis);
+                Console.WriteLine("Final Amount : " + final);
+            }
+            else
+            {
+                Console.WriteLine("----------Product Details----------");
+                Console.WriteLine("ID : " + pi);
+                Console.WriteLine("product Name : " + pn);
+                Console.WriteLine("Unit Price : " + up);
+                Console.WriteLine("Quantity : " + q);
+                Console.WriteLine("Total : " + total);
+                Console.WriteLine("Discount : Zero");
+                Console.WriteLine("Final Amount : " + total);
+            }
+            Console.ReadLine();
+        }
+    }
+    
+}
